27e6c1bc63128d077dcb38ba20b261e9bc1e5467
welt <welt@mac.com>
2024-11-26 13:36:36 +0000

    improves test coverage

--- tests/api.test.js
+++ tests/api.test.js
@@ -6,8 +6,13 @@ const consoleSpy = jest.spyOn(console, "error");
 const eleventyUri = "https://api.github.com/repos/11ty/eleventy";
 const customUri = "https://api.example.com";
 const invalidUri = "https://invalid.example.com";
+const mockCachedData = { loremFakeKey: "ipsumFakeValue" };
 
-describe("The API helper handles args", () => {
+describe("Test the API handles args", () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   beforeAll(() => {
     global.fetch = jest.fn(() =>
       Promise.resolve({
@@ -50,5 +55,63 @@ describe("The API helper handles args", () => {
   test("It logs any error", () => {
     const api = new Api(invalidUri);
     expect(consoleSpy).toBeCalled();
+  });
+
+  test("It should throw an error if the network response is not ok", async () => {
+    global.fetch.mockResolvedValue({
+      ok: false,
+      status: 500,
+      json: jest.fn().mockResolvedValue({}),
+    });
+    const api = new Api(customUri);
+    await expect(api.getData()).rejects.toThrow("Network response was not ok");
+  });
+
+  test("It should return data if the network response is ok", async () => {
+    const mockData = { loremFakeKey: "ipsumFakeValue" };
+    global.fetch.mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockData),
+    });
+    const api = new Api(customUri);
+    const data = await api.getData();
+    expect(data).toEqual(mockData);
+  });
+
+  test("It should set the _useCache property correctly", () => {
+    const api = new Api(customUri);
+    expect(api.useCache).toBe(true);
+    api.useCache = false;
+    expect(api.useCache).toBe(false);
+    api.useCache = "lorem-ipsum-dolor-)(*&^%$#@!";
+    expect(api.useCache).toBe(true);
+    api.useCache = true;
+    expect(api.useCache).toBe(true);
+  });
+
+  test("It should return cached data if useCache is true and data is cached", async () => {
+    const api = new Api(customUri, { useCache: true });
+    api.cache = {
+      getCachedData: jest.fn().mockReturnValue(mockCachedData),
+      setCachedData: jest.fn(),
+    };
+    const data = await api.getData();
+    expect(data).toEqual(mockCachedData);
+    expect(api.cache.getCachedData).toHaveBeenCalledWith(customUri);
+  });
+
+  test("It should not return cached data if useCache is false", async () => {
+    const api = new Api(customUri, { useCache: false });
+    api.cache = {
+      getCachedData: jest.fn().mockReturnValue(null),
+      setCachedData: jest.fn(),
+    };
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: jest.fn().mockResolvedValue(mockCachedData),
+    });
+    const data = await api.getData();
+    expect(data).toEqual(mockCachedData);
+    expect(api.cache.getCachedData).not.toHaveBeenCalled();
   });
 });
