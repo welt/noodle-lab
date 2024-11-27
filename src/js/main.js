a41ce9a2fccf85dad5bb81b920bb99c1c7fb0659
welt <welt@mac.com>
2024-11-27 22:06:03 +0000

    amends main.js to add new modules

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,29 @@
 import messages from "./_modules/messages.js";
-import LoadingSpinner from "./_components/loadingSpinner.js";
+import ToggleDarkMode from "./_lib/toggleDarkMode.js";
+
 import CarbonReporter from "./_components/carbonReporter.js";
 import GitHubReporter from "./_components/gitHubReporter.js";
+import LoadingSpinner from "./_components/loadingSpinner.js";
 import RandomReporter from "./_components/randomReporter.js";
+import ToggleButton from "./_components/toggleButton.js";
 
-messages.hello();
+function defineCustomElements() {
+  customElements.define("carbon-reporter", CarbonReporter);
+  customElements.define("github-reporter", GitHubReporter);
+  customElements.define("loading-spinner", LoadingSpinner);
+  customElements.define("random-reporter", RandomReporter);
+  customElements.define("toggle-button", ToggleButton);
+}
 
-customElements.define("loading-spinner", LoadingSpinner);
-customElements.define("carbon-reporter", CarbonReporter);
-customElements.define("github-reporter", GitHubReporter);
-customElements.define("random-reporter", RandomReporter);
+function initializeThemeSwitch() {
+  const themeSwitch = new ToggleDarkMode();
+  themeSwitch.init();
+}
+
+function init() {
+  defineCustomElements();
+  messages.hello();
+  initializeThemeSwitch();
+}
+
+init();
