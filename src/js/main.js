96fd5af43b893505bae5f01fa8ff1e840565284e
welt <welt@mac.com>
2025-07-29 16:09:10 +0100

    adds f1 component to homepage

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,7 @@ import GitHubReporter from "./_components/gitHubReporter.js";
 import LoadingSpinner from "./_components/loadingSpinner.js";
 import RandomReporter from "./_components/randomReporter.js";
 import WizardReporter from "./_components/wizardReporter.js";
+import F1Reporter from "./_components/f1Reporter.js";
 import ToggleButton from "./_components/toggleButton.js";
 import RefreshButton from "./_components/refreshButton.js";
 import WizardControls from "./_components/wizardControls.js";
@@ -23,6 +24,7 @@ function defineCustomElements() {
   customElements.define("loading-spinner", LoadingSpinner);
   customElements.define("random-reporter", RandomReporter);
   customElements.define("wizard-reporter", WizardReporter);
+  customElements.define("f1-reporter", F1Reporter);
   customElements.define("toggle-button", ToggleButton);
   customElements.define("refresh-button", RefreshButton);
   customElements.define("wizard-controls", WizardControls);
