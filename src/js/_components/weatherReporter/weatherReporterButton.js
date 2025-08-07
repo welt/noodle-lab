9aaa01293b8eb6ea98a116e3887928cd01ea1899
welt <welt@mac.com>
2025-08-07 14:58:08 +0100

    adds weather reporter button

---
+++ src/js/_components/weatherReporter/weatherReporterButton.js
@@ -0,0 +1,43 @@
+/**
+ * @file weatherReporterButton.js
+ */
+import eventMixin from "../../_mixins/eventMixin";
+import mixinApply from "../../_lib/mixinApply";
+
+const eventName = "add-weather-reporter";
+const styles = ["button", "button--add-weather"];
+
+export default class WeatherReporterButton extends HTMLElement {
+  constructor() {
+    super();
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render() {
+    this.classList.add(...styles);
+  }
+
+  onClick(event) {
+    console.log("clicked");
+    this.emit(eventName, event.target.textContent);
+  }
+
+  connectedCallback() {
+    this.render();
+    this.addEventListener("click", this.onClick);
+    if (
+      typeof window !== "undefined" &&
+      !window.WEATHER_REPORTER_FEATURE_ENABLED
+    ) {
+      this.setAttribute("disabled", "");
+      this.title = "WeatherReporter feature is disabled";
+      console.warn("WeatherReporterButton: Feature is disabled.");
+    }
+  }
+
+  disconnectedCallback() {
+    this.removeEventListener("click", this.onClick);
+  }
+}
+
+mixinApply(WeatherReporterButton, eventMixin);
