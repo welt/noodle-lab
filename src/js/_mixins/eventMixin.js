602f76996b841cfaf16ae33ced70703effd51f50
welt <welt@mac.com>
2025-08-10 15:05:21 +0100

    appeases eslint

--- src/js/_mixins/eventMixin.js
+++ src/js/_mixins/eventMixin.js
@@ -6,11 +6,11 @@ export default {
   /**
    * Adds event listener to an EventTarget.
    * @param {String} type - The event type to subscribe to.
-   * @param {Function} callback 
+   * @param {Function} callback
    */
   subscribe: function (type, callback) {
-    if (typeof this.addEventListener !== 'function') {
-      throw new Error('Object does not implement EventTarget interface');
+    if (typeof this.addEventListener !== "function") {
+      throw new Error("Object does not implement EventTarget interface");
     }
 
     this.addEventListener(type, callback);
@@ -18,11 +18,11 @@ export default {
   /**
    * Removes event listener from an EventTarget.
    * @param {String} type - The event type to unsubscribe from.
-   * @param {Function} callback 
+   * @param {Function} callback
    */
   unsubscribe: function (type, callback) {
-    if (typeof this.removeEventListener !== 'function') {
-      throw new Error('Object does not implement EventTarget interface');
+    if (typeof this.removeEventListener !== "function") {
+      throw new Error("Object does not implement EventTarget interface");
     }
 
     this.removeEventListener(type, callback);
@@ -32,9 +32,9 @@ export default {
    * @param {String} type
    * @param {Object} detail
    */
-  emit: function (type, detail = {}) {
-    if (typeof this.dispatchEvent !== 'function') {
-      throw new Error('Object does not implement EventTarget interface');
+  emit: function (type, detail) {
+    if (typeof this.dispatchEvent !== "function") {
+      throw new Error("Object does not implement EventTarget interface");
     }
 
     const event = new CustomEvent(`${type}`, {
