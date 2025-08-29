4b26e9f000b9cef4cc31ed961c3eb1d96218d3eb
welt <welt@mac.com>
2025-08-29 19:30:49 +0100

    adds class-vs-class duck-typing util

--- tests/isDuckType.test.js
+++ tests/isDuckType.test.js
@@ -1,7 +1,10 @@
-import isDuckType from "../src/js/_lib/isDucktype/isDuckType.js";
+import {
+  isDuckType,
+  isDuckTypeClass,
+  DuckTypingError,
+} from "../src/js/_lib/isDucktype/index.js";
 import NativeFetchApi from "../src/js/_components/weatherReporter/nativeFetchApi.js";
 import ApiContract from "../src/js/_contracts/apiContract.js";
-import { DuckTypingError } from "../src/js/_lib/isDucktype/errors.js";
 
 describe("isDuckType utility", () => {
   test("NativeFetchApi fulfills ApiContract contract", () => {
@@ -12,5 +15,16 @@ describe("isDuckType utility", () => {
   test("Throws DuckTypingError if contract is not fulfilled", () => {
     const badDuck = { uri: "https://example.com" }; // missing getData
     expect(() => isDuckType(badDuck, ApiContract)).toThrow(DuckTypingError);
+  });
+});
+
+describe("isDuckTypeClass utility", () => {
+  test("NativeFetchApi class fulfills ApiContract contract", () => {
+    expect(isDuckTypeClass(NativeFetchApi, ApiContract)).toBe(true);
+  });
+
+  test("Throws DuckTypingError if class contract is not fulfilled", () => {
+    class BadApi {}
+    expect(() => isDuckTypeClass(BadApi, ApiContract)).toThrow(DuckTypingError);
   });
 });
