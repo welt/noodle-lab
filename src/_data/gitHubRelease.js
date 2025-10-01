0d58e28a1ff4022a7edf335a3706edcc11dc9097
welt <welt@mac.com>
2025-10-01 20:41:09 +0100

    caches 11ty API data sources on build

--- src/_data/gitHubRelease.js
+++ src/_data/gitHubRelease.js
@@ -1,5 +1,5 @@
-import safeFetch from "../../scripts/safeFetch.js";
-import safeJson from "../../scripts/safeJson.js";
+import pkg from "../../package.json" with { type: "json" };
+import eleventyFetch from "@11ty/eleventy-fetch";
 
 const REPO_USER = process.env.REPO_USER;
 const REPO_NAME = process.env.REPO_NAME;
@@ -10,7 +10,32 @@ function githubApiUrl(strings, user, repo) {
 
 const url = githubApiUrl`https://api.github.com/repos/${REPO_USER}/${REPO_NAME}/tags`;
 
-export default async function gitHubRelease() {
-  const response = await safeFetch(url);
-  return await safeJson(response, url);
+/**
+ * Latest tag name from GitHub releases.
+ * @returns {string}
+ */
+export default async function () {
+  try {
+    const json = await eleventyFetch(url, {
+      duration: "2h",
+      type: "json",
+      fetchOptions: {
+        headers: {
+          "User-Agent": "node.js",
+        },
+      },
+    });
+    let releases;
+    if (Array.isArray(json)) {
+      releases = json.map((tag) => ({
+        name: tag.name,
+      }));
+    } else {
+      releases = [json];
+    }
+    const [latestTag] = releases;
+    return latestTag;
+  } catch (err) {
+    return { name: pkg.version ? `v${pkg.version}` : "unknown" };
+  }
 }
