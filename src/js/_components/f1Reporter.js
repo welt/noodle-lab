78a263da7e994efaa8a5af3aca786aa95473a269
Richard Weltman <richard.weltman@agentsoftware.net>
2025-11-11 22:03:33 +0000

    amends api call

--- src/js/_components/f1Reporter.js
+++ src/js/_components/f1Reporter.js
@@ -6,10 +6,14 @@ import Reporter from "../_contracts/reporter.js";
 import formatTimestamp from "../_lib/formatTimestamp.js";
 import textUtils from "../_lib/textUtils.js";
 
-const url = new URL("https://api.openf1.org/v1/meetings");
+const now = new Date();
+const fullYear = now.toISOString().slice(0, 10);
+const url = new URL("https://api.openf1.org/v1/sessions");
+
 const params = new URLSearchParams({
   year: "2025",
-  meeting_key: "latest",
+  session_name: "Race",
+  "date_end<": fullYear,
 });
 
 const styles = ["reporter", "reporter--f1"];
@@ -23,17 +27,27 @@ export default class F1Reporter extends Reporter {
    * @param {Array<JSON>} data - OpenF1 API response data.
    */
   render(data) {
-    const [oData] = data;
+    const past = data.filter(
+      (session) => session.date_end && new Date(session.date_end) <= now,
+    );
+
+    if (past.length === 0)
+      throw new Error("No completed races found yet this year.");
+
+    const last = past.reduce((a, b) =>
+      new Date(a.date_end) > new Date(b.date_end) ? a : b,
+    );
+
     this.classList.add(...styles);
     this.innerHTML = `
       <h2>Latest F1 Meeting</h2>
       <dl>
-        <dt>Meeting Name:</dt>
-        <dd>${oData.meeting_name}</dd>
-        <dt>Meeting Official Name:</dt>
-        <dd><em>${textUtils.toTitleCase(oData.meeting_official_name)}</em></dd>
+        <dt>Location:</dt>
+        <dd>${last.country_name}, ${last.location}</dd>
+        <dt>Circuit:</dt>
+        <dd><em>${textUtils.toTitleCase(last.circuit_short_name)}</em></dd>
         <dt>Meeting Date:</dt>
-        <dd>${formatTimestamp(oData.date_start)}</dd>
+        <dd>${formatTimestamp(last.date_start)}</dd>
       </dl>
     `;
   }
