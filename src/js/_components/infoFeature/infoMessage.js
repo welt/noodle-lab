a5d3ba5ea45e908b54d46da7cfdc5715248cd205
welt <welt@mac.com>
2025-12-15 08:47:21 +0000

    adds citation

--- src/js/_components/infoFeature/infoMessage.js
+++ src/js/_components/infoFeature/infoMessage.js
@@ -3,7 +3,7 @@
  * object with text and getText method
  */
 export default {
-  text: "All of the sadness of the city came suddenly with the first cold rains of winter, and there were no more tops to the high white houses as you walked but only the wet blackness of the street and the closed doors of the small shops, the herb sellers, the stationery and the newspaper shops, the midwife—second class—and the hotel where Verlaine had died where I had a room on the top floor where I worked.",
+  text: "All of the sadness of the city came suddenly with the first cold rains of winter, and there were no more tops to the high white houses as you walked but only the wet blackness of the street and the closed doors of the small shops, the herb sellers, the stationery and the newspaper shops, the midwife—second class—and the hotel where Verlaine had died where I had a room on the top floor where I worked. (Hemingway, Ernest: A Moveable Feast, Charles Scribner & Sons, New York, 1964)",
 
   getText() {
     return this.text;
